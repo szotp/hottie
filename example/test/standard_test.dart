94ece9b1a5493ee76da49750b02d807e6414e6e4
szotp <qwertyszot@gmail.com>
2020-08-26 20:11:51 +0200

    configurable failing

--- example/test/standard_test.dart
+++ example/test/standard_test.dart
@@ -3,9 +3,15 @@ import 'dart:io';
 import 'package:example/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+const shouldFail = bool.fromEnvironment('shouldFail');
+
 void main() {
   test('test add 1', () async {
-    expect(add(1, 2), 4);
+    expect(add(1, 2), 3);
+  });
+
+  test('test asserts', () async {
+    assert(!shouldFail);
   });
 
   test(
@@ -13,7 +19,7 @@ void main() {
     () async {
       final file = File('file.txt');
       final contents = file.readAsStringSync();
-      expect(contents, 'testt');
+      expect(contents, 'test');
     },
     tags: ["File"],
   );
